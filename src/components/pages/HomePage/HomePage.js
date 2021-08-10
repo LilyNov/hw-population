10fcb10ed7db969be39827e40d753455cbcffd14
Liliia Novikova <kplily85@gmail.com>
2021-08-10 16:28:18 +0300

    add chart for home-page

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react";
+import { fetchForHomePage } from '../../service/population-api'
+import StatisticBar from '../../StatisticBar/StatisticBar'
+
 const HomePage = () => {
+const [statistic, setStatistic] = useState([])
+    const [error, setError] = useState(null)
+    const [status, setStatus] = useState("idle");
+
+    useEffect(() => {
+        setStatus('pending');
+        fetchForHomePage().then(postStatistic => {
+            console.log(postStatistic);
+            setStatistic(postStatistic.data)
+            setStatus("resolved")
+        }).catch(error => {
+            setError(error)
+            setStatus("rejected")
+        })
+    }, [])
+
     return (
         <>
+            {status === "pending" && <p>LOADER</p>}
+            {status === "rejected" && <p>{error}</p>}
+            {status === "resolved" && <StatisticBar statistic={ statistic}/>}
         </>
     )
 }
---
+++ src/components/service/population-api.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://datausa.io/api/data?drilldowns"
+
+export const fetchForHomePage = () => fetch(`${BASE_URL}=Year&measures=Population`).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+export const fetchForStateYearPage = (query) => fetch(`${BASE_URL}=State&measures=Population`).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  })
